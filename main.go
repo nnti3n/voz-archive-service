685193d878c923a53f7d5afd85fe0a6dbc96b3d3
Nhat Tien <nntien@apcs.vn>
2017-12-07 22:46:57 +0700

    emit json when lauch

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/nnti3n/voz-archive-plus/utilities"
 	"github.com/nnti3n/voz-archive-plus/vozscrape"
@@ -14,7 +15,24 @@ func init() {
 
 // this is the console application
 func main() {
+	// connStr := "user=nntien dbname=pqgotest sslmode=verify-full"
+	// db, err := sql.Open("postgres", connStr)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	fmt.Println(Box())
+	f, err := os.Create("json.txt")
+	check(err)
+
+	content := Box()
+	n3, err := f.WriteString(content)
+	fmt.Print("wrote %d bytes\n", n3)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
 // Box will scape forum boxes
