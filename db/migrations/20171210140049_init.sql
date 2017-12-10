6246daaa908510fd2dfc5a3616d820af22483eb9
Nhat Tien <nntien@apcs.vn>
2017-12-10 14:11:59 +0700

    Init db

---
+++ db/migrations/20171210140049_init.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+
+CREATE TABLE boxes(id INTEGER PRIMARY KEY);
+
+CREATE TABLE threads(id INTEGER PRIMARY KEY, title TEXT not null, source TEXT, page_count INT not null, post_count INT not null, view_count INT not null, user_name VARCHAR(40), box_id INTEGER REFERENCES boxes not null);
+
+CREATE TABLE posts(id INTEGER PRIMARY KEY, user_id INTEGER not null, number INTEGER, user_name VARCHAR(40), content TEXT, time VARCHAR(40) not null, thread_id INTEGER REFERENCES threads not null);
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+
+DROP TABLE posts;
+DROP TABLE threads;
+DROP TABLE boxes;
