7ec7ea645fe0717a0866b26c1d1d8a7ec150970e
Nhat Tien <nntien@apcs.vn>
2017-12-20 22:26:18 +0700

    Box api

---
+++ interface/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	// "fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-pg/pg"
+	"github.com/nnti3n/voz-archive-plus/interface/requesthandler"
+	// "github.com/pkg/errors"
+)
+
+// func LoadConfiguration(pwd string) error {
+// 	viper.SetConfigName("voz-config")
+// 	viper.AddConfigPath(pwd)
+// 	devPath := pwd[:len(pwd)-3] + "interface/"
+// 	_, file, _, _ := runtime.Caller(1)
+// 	configPath := path.Dir(file)
+// 	viper.AddConfigPath(devPath)
+// 	viper.AddConfigPath(configPath)
+// 	return viper.ReadInConfig() // Find and read the config file
+// }
+
+func main() {
+	// flag.Parse()
+	// pwd, err := osext.ExecutableFolder()
+	// if err != nil {
+	// 	log.Fatalf("cannot retrieve present working directory: %i", 0600, nil)
+	// }
+
+	// err = LoadConfiguration(pwd)
+	// if err != nil {
+	// 	panic(errors.Errorf("Fatal reading config file: %s \n", err))
+	// }
+
+	// var db *pg.DB
+	// if dev == "true" {
+	// 	db = pg.Connect(fmt.Sprintf("host=%s port=%d user=%s "+
+	// 		"password=%s dbname=%s sslmode=disable",
+	// 		dbURL, dbPort, dbUser, dbPass, dbName))
+	// 	if err != nil {
+	// 		panic(errors.Errorf("Cannot connect to database: %s", err))
+	// 	}
+	// } else {
+	// 	db = pg.Connect(dbURL)
+	// 	if err != nil {
+	// 		panic(errors.Errorf("Cannot connect to database: %s", err))
+	// 	}
+	// }
+
+	db := pg.Connect(&pg.Options{
+		User:     "nntien",
+		Database: "vozarchive",
+	})
+
+	router := gin.Default()
+	env := requesthandler.Env{Db: db}
+	r := router.Group("/api")
+	{
+		// r.GET("/box", fetchAllBox)
+		r.GET("/box/:boxID", env.FetchAllThread)
+		r.GET("/thread/:threadID", env.FetchSingleThread)
+		// r.GET("/post/:postID", fetchSinglePost)
+	}
+	router.Run()
+}
---
+++ interface/requesthandler/forums.go
@@ -0,0 +1,59 @@
+package requesthandler
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/go-pg/pg"
+	"github.com/nnti3n/voz-archive-plus/serviceWorker/vozscrape"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Env store db
+type Env struct {
+	Db *pg.DB
+}
+
+// FetchAllThread fetch all threads
+func (e *Env) FetchAllThread(c *gin.Context) {
+	boxID := c.Param("boxID")
+
+	threads := []vozscrape.Thread{}
+	err := e.Db.Model(&threads).Limit(20).Select()
+	log.Println(c.Param("boxID"))
+	if err == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"data":   threads,
+			"params": boxID,
+		})
+	} else {
+		c.JSON(http.StatusNoContent, gin.H{
+			"data":   threads,
+			"params": boxID,
+		})
+	}
+
+}
+
+// FetchSingleThread fetch all posts of thread
+func (e *Env) FetchSingleThread(c *gin.Context) {
+	threadID, err := strconv.Atoi(c.Param("ThreadID"))
+
+	thread := vozscrape.Thread{ID: 6778933}
+	err = e.Db.Select(&thread)
+
+	if err == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"err":       err,
+			"querydata": threadID,
+			"data":      thread,
+		})
+	} else {
+		c.JSON(http.StatusNoContent, gin.H{
+			"querydata": threadID,
+			"data":      []string{},
+		})
+	}
+}
