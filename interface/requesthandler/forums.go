3207373adea21be68b092eb9528037b8f653df2b
Nhat Tien <nntien@apcs.vn>
2018-01-30 02:18:53 +0700

    Posts pagination

--- interface/requesthandler/forums.go
+++ interface/requesthandler/forums.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
 	"github.com/nnti3n/voz-archive-plus/serviceWorker/vozscrape"
 	"github.com/nnti3n/voz-archive-plus/utilities"
 
@@ -60,14 +61,34 @@ func (e *Env) FetchSingleThread(c *gin.Context) {
 	}
 }
 
+// PostFilter struct for filter
+type PostFilter struct {
+	orm.Pager
+	ThreadID int
+}
+
+// postFilter filter posts by id and page
+func (f *PostFilter) postFilter(q *orm.Query) (*orm.Query, error) {
+	if f.ThreadID > 0 {
+		q = q.Where("thread_id = ?", f.ThreadID)
+	}
+	q = q.Order("number ASC").Apply(f.Pager.Paginate)
+	return q, nil
+}
+
 // FetchThreadPosts fetch all posts of thread
 func (e *Env) FetchThreadPosts(c *gin.Context) {
 	threadID, _ := strconv.Atoi(c.Param("threadID"))
-	limit, offset := utilities.Pagination(c, 20)
+	// limit, offset := utilities.Pagination(c, 20)
+
+	var filter PostFilter
+	filter.Pager.SetURLValues(c.Request.URL.Query())
+	filter.ThreadID = threadID
 
 	posts := []vozscrape.Post{}
-	err := e.Db.Model(&posts).Where("thread_id = ?", threadID).
-		Offset(offset).Limit(limit).Order("number ASC").Select()
+	// err := e.Db.Model(&posts).Where("thread_id = ?", threadID).
+	// 	Offset(offset).Limit(limit).Order("number ASC").Select()
+	err := e.Db.Model(&posts).Apply(filter.postFilter).Select()
 
 	if err == nil {
 		c.JSON(http.StatusOK, gin.H{
