59111996978b63f0cae0641286f6b32ddcf0a807
Nhat Tien <nntien@apcs.vn>
2018-04-10 22:41:05 +0700

    format shorter

--- serviceWorker/vozscrape/thread.go
+++ serviceWorker/vozscrape/thread.go
@@ -41,7 +41,9 @@ var excludeThreads = []int{6735473, 6609261, 3613304, 2024506, 5523490}
 
 // NewThread creates a Thread and fills missing information
 // from the Thread page
-func NewThread(id int, title string, userID int, userName string, source string, pageCount string, postCount string, viewCount string, boxID int) *Thread {
+func NewThread(id int, title string, userID int, userName string,
+	source string, pageCount string, postCount string, viewCount string,
+	boxID int) *Thread {
 
 	t := new(Thread)
 	t.ID = id
@@ -81,37 +83,41 @@ func (t *Thread) getPosts(pPage []scraper.Scraper) []*Post {
 	posts := []*Post{}
 
 	for _, page := range pPage {
-		page.Find("#posts > div [align='left']").Each(func(i int, s *goquery.Selection) {
-			p := new(Post)
-			number := s.Find("tr:first-child td div:first-child a:first-child")
-			numberName, exist := number.Attr("name")
-			if !exist {
-				fmt.Println("Not found post number, set -1")
-				numberName = "-1"
-			}
-			p.Number, _ = strconv.Atoi(numberName)
-			_number, exist := number.Attr("href")
-			if !exist {
-				fmt.Println("no post href")
-				return
-			}
-			p.ID, _ = strconv.Atoi(strings.Split(strings.Split(_number, "=")[1], "&")[0])
-			p.Time = strings.TrimSpace(s.Find("tr:first-child td.thead div:nth-child(2)").Text())
+		page.Find("#posts > div [align='left']").
+			Each(func(i int, s *goquery.Selection) {
+				p := new(Post)
+				number := s.Find("tr:first-child td div:first-child a:first-child")
+				numberName, exist := number.Attr("name")
+				if !exist {
+					fmt.Println("Not found post number, set -1")
+					numberName = "-1"
+				}
+				p.Number, _ = strconv.Atoi(numberName)
+				_number, exist := number.Attr("href")
+				if !exist {
+					fmt.Println("no post href")
+					return
+				}
+				p.ID, _ = strconv.
+					Atoi(strings.Split(strings.Split(_number, "=")[1], "&")[0])
+				p.Time = strings.
+					TrimSpace(s.Find("tr:first-child td.thead div:nth-child(2)").
+						Text())
 
-			userID, exist := s.Find(".bigusername").Attr("href")
-			if !exist {
-				fmt.Println("not found userID")
-				p.UserID = -1
-			} else {
-				p.UserID, _ = strconv.Atoi(strings.Split(userID, "u=")[1])
-			}
-			p.UserName = strings.TrimSpace(s.Find(".bigusername").Text())
-			_content, _ := s.Find(".voz-post-message").Html()
-			p.Content = strings.TrimSpace(_content)
-			p.ThreadID = t.ID
+				userID, exist := s.Find(".bigusername").Attr("href")
+				if !exist {
+					fmt.Println("not found userID")
+					p.UserID = -1
+				} else {
+					p.UserID, _ = strconv.Atoi(strings.Split(userID, "u=")[1])
+				}
+				p.UserName = strings.TrimSpace(s.Find(".bigusername").Text())
+				_content, _ := s.Find(".voz-post-message").Html()
+				p.Content = strings.TrimSpace(_content)
+				p.ThreadID = t.ID
 
-			posts = append(posts, p)
-		})
+				posts = append(posts, p)
+			})
 	}
 
 	return posts
@@ -125,7 +131,9 @@ func (t *Thread) fetchThread(currentPageCount int) []scraper.Scraper {
 		log.Println("Count", currentPageCount, t.PageCount)
 	}
 	for i <= t.PageCount {
-		p := scraper.NewScraper("https://vozforums.com/showthread.php?t="+strconv.Itoa(t.ID)+"&page="+strconv.Itoa(i), "utf-8")
+		p := scraper.
+			NewScraper("https://vozforums.com/showthread.php?t="+
+				strconv.Itoa(t.ID)+"&page="+strconv.Itoa(i), "utf-8")
 		s = append(s, *p)
 		i++
 	}
